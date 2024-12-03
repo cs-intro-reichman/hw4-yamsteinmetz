d4870c4ddbebd8eeaca2f3aabbbfd8cb4d5a4919
yamsteinmetz <yam.steinmetz@post.runi.ac.il>
2024-12-03 15:00:17 +0200

    home work 4

--- MyString.java
+++ MyString.java
@@ -16,22 +16,38 @@ public class MyString {
         System.out.println(contains("resignation", "sign")); // true
     }
 
-    /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-       if (str.length()==0){
-        return null;
-    }
-    String result = "";
-    for (int i=0; i<str.length();i++){
-        char c = str.charAt(i);
-        if (c>='A' && c<= 'Z'){
-
+        if (str.length() == 0) {
+            return "";
         }
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                c = (char) (c + 32);
+            }
+            result += c; // פשוט משתמשים ב-+= כדי להוסיף את התו
+        }
+        return result;
     }
 
-    /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        if (str1.length() < str2.length()) {
+            return false;
+        }
+        
+        for (int i = 0; i <= str1.length() - str2.length(); i++) {
+            boolean match = true;
+            for (int j = 0; j < str2.length(); j++) {
+                if (str1.charAt(i + j) != str2.charAt(j)) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                return true;
+            }
+        }
         return false;
     }
 }
